a60ec05196cfe6d4bf0dee8773e96203cb67d793
dalf2598 <alejofon98@hotmail.com>
2024-03-03 13:50:09 -0500

    test: added tests for get_movies function

---
+++ test/get-movies.spec.js
@@ -0,0 +1,30 @@
+import { jest } from "@jest/globals";
+import { OPTIONS, API_URL } from "../javascript/constants.js";
+import { get_movies } from "../javascript/get-movies.js";
+import mockApiResponse from "./mocks/get-movies-api-response.json";
+describe("get_movies function", () => {
+  afterEach(() => {
+    global.fetch.mockClear();
+  });
+  it("should fetch movies successfully", async () => {
+    const mockResults = mockApiResponse.results;
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        text: () => Promise.resolve(`{"results": ${JSON.stringify(mockResults)}}`),
+      })
+    );
+    const movies = await get_movies();
+    expect(movies).toEqual(mockResults);
+    expect(fetch).toHaveBeenCalledWith(API_URL, OPTIONS);
+  });
+
+  it("should print an error to console when fails to fetch movies", async () => {
+    const mockFailureMessage = "You are not subscribed to this API";
+    global.fetch = jest.fn(() => Promise.reject(mockFailureMessage));
+    const consoleErrorMock = jest
+      .spyOn(console, "error")
+      .mockImplementation(() => jest.fn());
+    await get_movies();
+    expect(consoleErrorMock).toHaveBeenCalledWith(mockFailureMessage);
+  });
+});
